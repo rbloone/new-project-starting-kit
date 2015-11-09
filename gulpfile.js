c5d08e057d4f492f1ed723386a92b82f7b1d7125
Roman <kondratyev@balticpay.eu>
2015-11-09 15:10:23 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,102 @@
+/*=============================================
+=            Gulp Starter by @dope            =
+=============================================*/
+
+/**
+*
+* The packages we are using
+* Not using gulp-load-plugins as it is nice to see whats here.
+*
+**/
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var browserSync  = require('browser-sync');
+var prefix       = require('gulp-autoprefixer');
+var plumber      = require('gulp-plumber');
+var uglify       = require('gulp-uglify');
+var rename       = require("gulp-rename");
+var imagemin     = require("gulp-imagemin");
+var pngquant     = require('imagemin-pngquant');
+
+/**
+*
+* Styles
+* - Compile
+* - Compress/Minify
+* - Catch errors (gulp-plumber)
+* - Autoprefixer
+*
+**/
+gulp.task('sass', function() {
+  gulp.src('assets/sass/**/*.scss')
+  .pipe(sass({
+    outputStyle: 'compressed',
+    // includePaths: require('node-normalize-scss').with('other/path', 'another/path') 
+    // - or - 
+    includePaths: require('node-normalize-scss').includePaths}))
+  .pipe(prefix('last 2 versions', '> 1%', 'ie 8', 'Android 2', 'Firefox ESR'))
+  .pipe(plumber())
+  .pipe(gulp.dest('assets/stylesheets'));
+});
+
+/**
+*
+* BrowserSync.io
+* - Watch CSS, JS & HTML for changes
+* - View project at: localhost:3000
+*
+**/
+gulp.task('browser-sync', function() {
+  browserSync.init(['assets/stylesheets/*.css', 'assets/js/**/*.js', 'index.html'], {
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+
+/**
+*
+* Javascript
+* - Uglify
+*
+**/
+gulp.task('scripts', function() {
+  gulp.src('assets/js/*.js')
+  .pipe(uglify())
+  .pipe(rename({
+    dirname: "min",
+    suffix: ".min",
+  }))
+  .pipe(gulp.dest('assets/js'))
+});
+
+/**
+*
+* Images
+* - Compress them!
+*
+**/
+gulp.task('images', function () {
+  return gulp.src('assets/images/*')
+  .pipe(imagemin({
+    progressive: true,
+    svgoPlugins: [{removeViewBox: false}],
+    use: [pngquant()]
+  }))
+  .pipe(gulp.dest('assets/images'));
+});
+
+
+/**
+*
+* Default task
+* - Runs sass, browser-sync, scripts and image tasks
+* - Watchs for file changes for images, scripts and sass/css
+*
+**/
+gulp.task('default', ['sass', 'browser-sync', 'scripts', 'images'], function () {
+  gulp.watch('assets/sass/**/*.scss', ['sass']);
+  gulp.watch('assets/js/**/*.js', ['scripts']);
+  gulp.watch('assets/images/*', ['images']);
+});
